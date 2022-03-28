6c5b97820f03fe5b36596cc369a3ef27ffc38de5
aivxx <90653195+aivxx@users.noreply.github.com>
2022-03-28 18:04:01 -0400

    create scriptable objects
    create script to align interactables with hands

--- Assets/Scripts/HandControl.cs
+++ Assets/Scripts/HandControl.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[ExecuteInEditMode]
 public class HandControl : MonoBehaviour
 {
+
+    public static void AlignHandToAttachment(Transform handroot, Transform handGrabAttach, Transform interactableAttach)
+    {
+        handroot.rotation = interactableAttach.rotation * Quaternion.Inverse(handGrabAttach.localRotation);
+        handroot.position = interactableAttach.position + (handroot.position - handGrabAttach.position);
+    }
+
+
     public bool hideHand = false;
+
 
 }
