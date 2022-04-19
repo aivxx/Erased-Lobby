41a867e3310a35fb434a9cf976f7aaa0c5ebfec3
aivxx <90653195+aivxx@users.noreply.github.com>
2022-04-19 19:33:10 -0400

    debugging grab interactables

---
+++ Assets/Scripts/LogInteractions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LogInteractions : MonoBehaviour
+{
+    public void LogHoverEnter(XRBaseInteractor interactor)
+    {
+        Debug.Log(gameObject.name + "Hover Enter by: " + interactor.gameObject.name);
+    }
+
+    public void LogHoverExit(XRBaseInteractor interactor)
+    {
+        Debug.Log(gameObject.name + "Hover Exit by: " + interactor.gameObject.name);
+    }
+
+    public void LogSelect(XRBaseInteractor interactor)
+    {
+        Debug.Log(gameObject.name + "Selected by: " + interactor.gameObject.name);
+    }
+
+    public void LogDeselect(XRBaseInteractor interactor)
+    {
+        Debug.Log(gameObject.name + "Deselected by: " + interactor.gameObject.name);
+    }
+
+    public void LogActivate(XRBaseInteractor interactor)
+    {
+        Debug.Log(gameObject.name + "Activated by: " + interactor.gameObject.name);
+    }
+
+    public void LogDeActivated(XRBaseInteractor interactor)
+    {
+        Debug.Log(gameObject.name + "Deactivated by: " + interactor.gameObject.name);
+    }
+
+
+}
