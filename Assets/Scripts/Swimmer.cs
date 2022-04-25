c83dd43bf308ceb67114f857adec50de23d894f9
aivxx <90653195+aivxx@users.noreply.github.com>
2022-04-25 18:54:55 -0400

    remove unnecessary proj files, change swimming script

--- Assets/Scripts/Swimmer.cs
+++ Assets/Scripts/Swimmer.cs
@@ -10,82 +10,92 @@ public class Swimmer : MonoBehaviour
     [Header("Values")]
     [SerializeField] float swimForce = 2f;
     [SerializeField] float dragForce = 1f;
-    [SerializeField] float minForce;
-    [SerializeField] float minTimeBetweenStrokes;
+    [SerializeField] float minForce = 1.1f;
+    [SerializeField] float minTimeBetweenStrokes = 0f;
 
     [Header("References")]
     [SerializeField] InputActionReference leftControllerSwimReference;
     [SerializeField] InputActionReference leftControllerVelocity;
     [SerializeField] InputActionReference rightControllerSwimReference;
     [SerializeField] InputActionReference rightControllerVelocity;
-    [SerializeField] Transform trackingReference;
 
-    Rigidbody _rigidbody;
-    float _cooldownTimer;
-    
+    [SerializeField] Transform trackingDReference;
 
-    private void Awake()
+    private Rigidbody _rigidBody;
+    private float _cooldownTimer;
+    private bool _swimEnabled = false;
+
+
+    void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
-
-    }
-
-    private void FixedUpdate()
-    {
-
-            
-    
+        _rigidBody = GetComponent<Rigidbody>();
        
+        _rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        if (!_swimEnabled)
+        {
+            return;
+        }
+        _cooldownTimer += Time.deltaTime;
+
+        if (_cooldownTimer > minTimeBetweenStrokes && leftControllerSwimReference.action.IsPressed() && rightControllerSwimReference.action.IsPressed())
+        {
+            var leftHandVelocity = leftControllerVelocity.action.ReadValue<Vector3>();
+            var rightHandVelocty = rightControllerVelocity.action.ReadValue<Vector3>();
+
+            Vector3 localVelocity = -1 * (leftHandVelocity + rightHandVelocty);
+
+            if (localVelocity.sqrMagnitude > minForce * minForce)
+            {
+                Vector3 worldVelocity = trackingDReference.TransformDirection(localVelocity);
+                _rigidBody.AddForce(worldVelocity * swimForce, ForceMode.Acceleration);
+                _cooldownTimer = 0f;
+            }
+        }
+
+        if (_rigidBody.velocity.sqrMagnitude > 0.01f)
+        {
+            _rigidBody.AddForce(-_rigidBody.velocity * dragForce, ForceMode.Acceleration);
+        }
 
 
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-          if (collision.gameObject.tag == "Water")
+        if (collision.gameObject.name == "Water")
         {
-            Debug.Log("Collision Enter");
-            _cooldownTimer += Time.fixedDeltaTime;
-            _rigidbody.useGravity = false;
-            //add velocity when both controllers pressed
-            if (_cooldownTimer > minTimeBetweenStrokes
-                && leftControllerSwimReference.action.IsPressed()
-                && rightControllerSwimReference.action.IsPressed())
-            {
-                var leftHandVelocity = leftControllerVelocity.action.ReadValue<Vector3>();
-                var rightHandVelocity = rightControllerVelocity.action.ReadValue<Vector3>();
-                Vector3 localVelocity = leftHandVelocity + rightHandVelocity;
-                localVelocity *= -1;
-
-                //swim forward
-                if (localVelocity.sqrMagnitude > minForce * minForce)
-                {
-                    Vector3 worldVelocity = trackingReference.TransformDirection(localVelocity);
-                    _rigidbody.AddForce(worldVelocity * swimForce, ForceMode.Acceleration);
-                    _cooldownTimer = 0f;
-                }
-
-            }
-
-            //apply water drag force when player is moving
-            if (_rigidbody.velocity.sqrMagnitude > 0.01f)
-            {
-                _rigidbody.AddForce(-_rigidbody.velocity * dragForce, ForceMode.Acceleration);
-            }
-           
+            _swimEnabled = true;
+            _rigidBody.useGravity = false;
         }
     }
-
 
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.name == "Water")
         {
-            _rigidbody.useGravity = true;
+            _swimEnabled = false;
+            _rigidBody.useGravity = true;
 
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+           
+        }
+    }
 }
